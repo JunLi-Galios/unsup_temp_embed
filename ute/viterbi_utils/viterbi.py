38d5f7b8b0d1d8ea5fd7818f576ea2859c61b853
Jun Li <junli19860919@gmail.com>
2020-12-18 16:52:21 -0800

    Update viterbi.py

--- ute/viterbi_utils/viterbi.py
+++ ute/viterbi_utils/viterbi.py
@@ -1,11 +1,13 @@
 import numpy as np
 from os.path import join
 from ute.utils.arg_pars import opt
+from ute.utils.logging_setup import logger
 
 
 class Viterbi:
-    def __init__(self, grammar, probs, transition=0.5):
+    def __init__(self, grammar, probs, transition=0.5, framesampling=1):
         self._grammar = grammar
+        self._framesampling = framesampling
         self._transition_self = -np.log(transition)
         self._transition_next = -np.log(1 - transition)
         self._transitions = np.array([self._transition_self, self._transition_next])
@@ -16,42 +18,61 @@ class Viterbi:
         self._probs = probs
         self._state = self._probs[0, 0]
         self._number_frames = self._probs.shape[0]
+        self._n_segs = int(np.ceil(self._number_frames/self._framesampling))
 
         # probabilities matrix
-        self._T1 = np.zeros((len(self._grammar), self._number_frames)) + np.inf
-        self._T1[0, 0] = self._state
+        self._T1 = np.zeros((len(self._grammar), self._n_segs)) + np.inf
+        start_state = self._grammar.states()[0]
+        self._seg_idx = 0
+        self._T1[start_state, 0] = self.get_prob(start_state)
         # argmax matrix
-        self._T2 = np.zeros((len(self._grammar), self._number_frames)) + np.inf
-        self._T2[0, 0] = 0
+        self._T2 = np.zeros((len(self._grammar), self._n_segs)) + np.inf
+        self._T2[start_state, 0] = 0
 
         self._frame_idx = 1
+        self._seg_idx = 1
 
     # @timing
     def inference(self):
-        while self._frame_idx < self._number_frames:
+        while self._seg_idx < self._n_segs:
             for state_idx, state in enumerate(self._grammar.states()):
                 idxs = np.array([max(state_idx - 1, 0), state_idx])
-                probs = self._T1[idxs, self._frame_idx - 1] + \
+                probs = self._T1[idxs, self._seg_idx - 1] + \
                         self._transitions[idxs - max(state_idx - 1, 0)] + \
                         self.get_prob(state)
-                self._T1[state_idx, self._frame_idx] = np.min(probs)
-                self._T2[state_idx, self._frame_idx] = np.argmin(probs) + \
+                self._T1[state_idx, self._seg_idx] = np.min(probs)
+                self._T2[state_idx, self._seg_idx] = np.argmin(probs) + \
                                                        max(state_idx - 1, 0)
-            self._frame_idx += 1
+            self._seg_idx += 1
 
     def get_prob(self, state):
-        return self._probs[self._frame_idx, state]
+        start_idx = self._seg_idx * self._framesampling
+        end_idx = min((self._seg_idx + 1) * self._framesampling, self._number_frames)
+        return np.sum(self._probs[start_idx:end_idx, state], axis=0)
+
+    # # @timing
+    # def backward(self, strict=True):
+    #     if strict:
+    #         last_state = -1 if self._T2.shape[0] < self._T2.shape[1] else self._T2.shape[1]
+    #         self._grammar.set_framewise_state(self._T2[last_state, -1], last=True)
+    #     else:
+    #         self._grammar.set_framewise_state(self._T1[..., -1], last=True)
+
+    #     for i in range(self._T1.shape[1] - 1, 0, -1):
+    #         self._grammar.set_framewise_state(self._T2[..., i])
+    #     self._grammar.reverse()
 
     # @timing
     def backward(self, strict=True):
+        last_iter = self._number_frames - (self._n_segs - 1) * self._framesampling
         if strict:
             last_state = -1 if self._T2.shape[0] < self._T2.shape[1] else self._T2.shape[1]
-            self._grammar.set_framewise_state(self._T2[last_state, -1], last=True)
+            self._grammar.set_framewise_state(self._T2[last_state, -1], last_iter, last=True)
         else:
-            self._grammar.set_framewise_state(self._T1[..., -1], last=True)
+            self._grammar.set_framewise_state(self._T1[..., -1], last_iter, last=True)
 
         for i in range(self._T1.shape[1] - 1, 0, -1):
-            self._grammar.set_framewise_state(self._T2[..., i])
+            self._grammar.set_framewise_state(self._T2[..., i], self._framesampling)
         self._grammar.reverse()
 
     def save_framewise_states(self):
