8868417f6c903227674510bf209718ec9d2f85cf
Jun Li <junli19860919@gmail.com>
2020-12-18 16:50:53 -0800

    Update grammar.py

--- ute/viterbi_utils/grammar.py
+++ ute/viterbi_utils/grammar.py
@@ -1,65 +1,87 @@
-#!/usr/bin/env python
-
-"""Grammar implementation for the viterbi decoding. Object should be assign to
-each video to collect the states for each of the frames.
-"""
-
-__author__ = 'Anna Kukleva'
-__date__ = 'September 2018'
-
-
 import numpy as np
 
 
-class Grammar:
-    def __init__(self, states):
-        """
-        Args:
-            states: flat sequence (list) of states (class State)
-        """
-        self._states = states
-        self._framewise_states = []
-        self.name = '%d' % len(states)
+class Grammar(object):
+    
+    # @context: tuple containing the previous label indices
+    # @label: the current label index
+    # @return: the log probability of label given context p(label|context)
+    def score(self, context, label): # score is a log probability
+        return 0.0
 
-    def framewise_states(self):
-        return_states = list(map(lambda x: self._states[x], self._framewise_states))
-        return return_states
+    # @return: the number of classes
+    def n_classes(self):
+        return 0
 
-    def reverse(self):
-        self._framewise_states = list(reversed(self._framewise_states))
+    # @return sequence start symbol
+    def start_symbol(self):
+        return -1
 
-    def __getitem__(self, idx):
-        return self._framewise_states[idx]
+    # @return sequence end symbol
+    def end_symbol(self):
+        return -2
 
-    def set_framewise_state(self, states, last=False):
-        """Set states for each item in a sequence.
-        Backward pass by setting a particular state for computed probabilities.
-        Args:
-            states: either state indexes of the previous step
-            last: if it the last item or not
+    # @context: tuple containing the previous label indices
+    # @return: list of all possible successor labels for the given context
+    def possible_successors(context):
+        return set()
 
-        Returns:
+# grammar containing all action transcripts seen in training
+# used for inference
+class PathGrammar(Grammar):
+    
+    def __init__(self, transcript_file, label2index_map):
+        self.num_classes = len(label2index_map)
+        transcripts = self._read_transcripts(transcript_file, label2index_map)
+        # generate successor sets
+        self.successors = dict()
+        for transcript in transcripts:
+            transcript = transcript + [self.end_symbol()]
+            for i in range(len(transcript)):
+                context = (self.start_symbol(),) + tuple(transcript[0:i])
+                self.successors[context] = set([transcript[i]]).union( self.successors.get(context, set()) )
 
-        """
-        if not last:
-            state = int(states[[self._framewise_states[-1]]])
+    def _read_transcripts(self, transcript_file, label2index_map):
+        transcripts = []
+        with open(transcript_file, 'r') as f:
+            lines = f.read().split('\n')[0:-1]
+        for line in lines:
+            transcripts.append( [ label2index_map[label] for label in line.split() ] )
+        return transcripts
+
+    def n_classes(self):
+        return self.num_classes
+
+    def possible_successors(self, context):
+        return self.successors.get(context, set())
+
+    def score(self, context, label):
+        if label in self.possible_successors(context):
+            return 0.0
         else:
-            # state = int(self._states[-1])
-            state = int(len(self._states) - 1)
-
-        self._framewise_states.append(state)
-
-    def states(self):
-        return self._states
-
-    def __len__(self):
-        return len(self._states)
+            return -np.inf
 
 
-def create_grammar(n_states):
-    """Create grammar out of given number of possible states with 1 sub-action
-    each.
-    """
-    states = list(range(n_states))
-    grammar = Grammar(states)
-    return grammar
+# grammar that generates only a single transcript
+# use during training to align frames to transcript
+class SingleTranscriptGrammar(Grammar):
+
+    def __init__(self, transcript, n_classes):
+        self.num_classes = n_classes
+        transcript = transcript + [self.end_symbol()]
+        self.successors = dict()
+        for i in range(len(transcript)):
+            context = (self.start_symbol(),) + tuple(transcript[0:i])
+            self.successors[context] = set([transcript[i]]).union( self.successors.get(context, set()) )
+
+    def n_classes(self):
+        return self.num_classes
+
+    def possible_successors(self, context):
+        return self.successors.get(context, set())
+
+    def score(self, context, label):
+        if label in self.possible_successors(context):
+            return 0.0
+        else:
+            return -np.inf
